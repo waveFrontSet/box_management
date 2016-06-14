059379cf4b8edc875765e500c98561abc688eaf4
waveFrontSet <paul.bubenzer@gmail.com>
2016-06-14 23:56:33 +0200

    Added proposed logic for models.

--- boxes/models.py
+++ boxes/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
-class Item(models.Model):
-    pass
+from box_management.users.models import User
 
-class ItemCategory(models.Model):
-    pass
+from model_utils.models import TimeStampedModel
+from model_utils import Choices
 
-class Box(models.Model):
-    pass
+
+class Item(TimeStampedModel):
+    STATUS = Choices('contained', 'taken by user')
+    name = models.CharField()
+    description = models.TextField(blank=True, null=True)
+    box = models.ForeignKey(Box, on_delete=models.CASCADE)
+    category = models.ForeignKey(ItemCategory, on_delete=models.CASCADE)
+    status = models.CharField(choices=STATUS, default=STATUS.contained, max_length=20)
+    in_possession_of = models.ForeignKey(User, blank=True, null=True)
+
+
+class ItemCategory(TimeStampedModel):
+    name = models.CharField()
+
+
+class Box(TimeStampedModel):
+    name = models.CharField()
+    location = models.CharField()
